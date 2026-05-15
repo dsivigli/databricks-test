0659afe8f00a186a097ebc0d185cc87f4f93f1d2
m.siviglia@libero.it <m.siviglia@libero.it>
2026-05-15 07:29:15 +0000

    sfd

--- synthetic_sales.py
+++ synthetic_sales.py
@@ -40,6 +40,7 @@ NUM_TRANSACTIONS = 1_000_000
 
 # COMMAND ----------
 
+# DBTITLE 1,Cell 4
 dim_store = (
     spark.range(1, NUM_STORES + 1)
     .withColumnRenamed("id", "store_id")
@@ -47,21 +48,21 @@ dim_store = (
         "country",
         F.element_at(
             F.array(F.lit("US"), F.lit("CA"), F.lit("MX"), F.lit("UK"), F.lit("DE"), F.lit("FR"), F.lit("JP")),
-            (F.pmod(F.hash("store_id"), F.lit(7)) + F.lit(1)).cast("int"),
+            (F.pmod(F.hash("store_id"), F.lit(7)) + F.lit(1))% NUM_STORES + 1
         ),
     )
     .withColumn(
         "region",
         F.element_at(
             F.array(F.lit("North"), F.lit("South"), F.lit("East"), F.lit("West"), F.lit("Central")),
-            (F.pmod(F.hash(F.col("store_id"), F.lit("region")), F.lit(5)) + F.lit(1)).cast("int"),
+            (F.pmod(F.hash(F.col("store_id"), F.lit("region")), F.lit(5)) + F.lit(1))% NUM_STORES + 1
         ),
     )
     .withColumn(
         "store_type",
         F.element_at(
             F.array(F.lit("flagship"), F.lit("standard"), F.lit("outlet"), F.lit("popup")),
-            (F.pmod(F.hash(F.col("store_id"), F.lit("type")), F.lit(4)) + F.lit(1)).cast("int"),
+            (F.pmod(F.hash(F.col("store_id"), F.lit("type")), F.lit(4)) + F.lit(1))% NUM_STORES + 1
         ),
     )
 )
@@ -86,7 +87,7 @@ dim_product = (
                 F.lit("electronics"), F.lit("apparel"), F.lit("grocery"), F.lit("home"),
                 F.lit("toys"), F.lit("beauty"), F.lit("sports"), F.lit("books"),
             ),
-            (F.pmod(F.hash("product_id"), F.lit(8)) + F.lit(1)).cast("int"),
+            (F.pmod(F.hash("product_id"), F.lit(8)) + F.lit(1))% NUM_PRODUCTS + 1
         ),
     )
     .withColumn(
@@ -117,7 +118,7 @@ dim_customer = (
         "loyalty_tier",
         F.element_at(
             F.array(F.lit("bronze"), F.lit("silver"), F.lit("gold"), F.lit("platinum")),
-            (F.pmod(F.hash("customer_id"), F.lit(4)) + F.lit(1)).cast("int"),
+            (F.pmod(F.hash("customer_id"), F.lit(4)) + F.lit(1))% NUM_CUSTOMERS + 1
         ),
     )
     .withColumn(
@@ -142,7 +143,7 @@ dim_promotion = (
         "promotion_type",
         F.element_at(
             F.array(F.lit("bogo"), F.lit("percent_off"), F.lit("flat_off"), F.lit("clearance"), F.lit("loyalty")),
-            (F.pmod(F.hash("promotion_id"), F.lit(5)) + F.lit(1)).cast("int"),
+            (F.pmod(F.hash("promotion_id"), F.lit(5)) + F.lit(1)) %NUM_PROMOTIONS+1
         ),
     )
     .withColumn("discount_pct", F.round((F.rand(seed=22) * F.lit(0.5)).cast("double"), 3))
